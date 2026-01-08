26952ed21b461a0082c4ee4715d758aedaa56ba9
Michael Hwang <michaelinjeyhwang@gmail.com>
2026-01-08 19:22:02 +0000

    refactor offset format style

--- CodeBreaker/Views/TimerView.swift
+++ CodeBreaker/Views/TimerView.swift
@@ -8,27 +8,23 @@
 import SwiftUI
 
 struct TimerView: View {
+    // MARK: Data in
     let startTime: Date
     let endTime: Date?
 
     var body: some View {
         if let endTime {
-            Text(
-                endTime,
-                format:
-                        .offset(
-                            to: startTime,
-                            allowedFields: [.minute, .second]
-                        )
-            )
+            Text(endTime, format: timerFormatStyle)
         } else {
-            Text(TimeDataSource<Date>.currentDate, format: .offset(
-                to: startTime,
-                allowedFields: [.minute, .second]
-            ))
+            Text(TimeDataSource<Date>.currentDate, format: timerFormatStyle)
         }
     }
+
+    private var timerFormatStyle: SystemFormatStyle.DateOffset {
+        .offset(to: startTime, allowedFields: [.minute, .second])
+    }
 }
+
 
 #Preview {
     TimerView(startTime: .now, endTime: nil)
